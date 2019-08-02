5e79fea893e3ce06280e8e0e2d71956d5a8dc8c6
Mat Anderson <matandersonlv@hotmail.com>
2019-08-02 00:07:33 +0000

    added Models/Users and some erbs files

--- app/controllers/emotions_controller.rb
+++ app/controllers/emotions_controller.rb
@@ -1,3 +1,12 @@
 class EmotionsController < ApplicationController
   
+  get '/emotions' do
+    if logged_in?
+      @tweets = Tweet.all
+      erb :'emotions/emotions'
+    else
+      redirect to '/login'
+    end
+  end
+  
 end
