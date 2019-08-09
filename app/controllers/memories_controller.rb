609e50609e306c1e218826166095414513b97cd3
Mat Anderson <matandersonlv@hotmail.com>
2019-08-09 20:46:09 +0000

    added add a Memory button to memories/index.erb

--- app/controllers/memories_controller.rb
+++ app/controllers/memories_controller.rb
@@ -4,30 +4,33 @@ class MemoriesController < ApplicationController
     @memories = Memory.all
     erb :'memories/index'
   end
-
-  post '/memories' do
-    @memory = current_user.memories.create(name: params[:name], content: params[:content])
-    @memory.emotion_ids = params[:emotions]
-    @memory.save
-
-    redirect to "memories/#{@memory.slug}"
-  end
-
+  
   get '/memories/new' do
     @memories = Memory.all
     @emotions = Emotion.all
     erb :'memories/new'
   end
 
+  post '/memories' do
+    if params[:name] == "" || params[:content] == ""
+      redirect to 'memories/new'
+    end
+    @memory = current_user.memories.create(name: params[:name], content: params[:content])
+    @memory.emotion_ids = params[:emotions]
+    @memory.save
+
+    redirect to "memories/#{@memory.slug}"
+  end
+  
+  get '/memories/:slug' do
+    @memory = Memory.find_by_slug(params[:slug])
+    erb :'memories/show'
+  end
+
   get '/memories/:slug/edit' do
     @memory = Memory.find_by_slug(params[:slug])
     @emotions = Emotion.all
     erb :'memories/edit'
-  end
-
-  get '/memories/:slug' do
-    @memory = Memory.find_by_slug(params[:slug])
-    erb :'memories/show'
   end
 
   patch '/memories/:slug' do
