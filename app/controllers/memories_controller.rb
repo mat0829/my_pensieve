1d2b38846326a54448262121709741b0da4d2dab
Mat Anderson <matandersonlv@hotmail.com>
2019-08-10 22:48:21 +0000

    added logged_in? to memories_controller.rb

--- app/controllers/memories_controller.rb
+++ app/controllers/memories_controller.rb
@@ -1,61 +1,88 @@
 class MemoriesController < ApplicationController
   
   get '/memories' do
-    @memories = Memory.all
-    erb :'memories/index'
+    if logged_in?
+      @memories = Memory.all
+      erb :'memories/index'
+    else
+      redirect to '/login'
+    end
   end
   
   get '/memories/new' do
-    @emotions = Emotion.all
-    erb :'memories/new'
+    if logged_in?
+      @emotions = Emotion.all
+      erb :'memories/new'
+    else
+      redirect to '/login'
+    end
   end
   
   post '/memories' do
-    binding.pry
-    if params[:name] == "" || params[:content] == ""
-      redirect to "/memories/new"
-    else
-      @memory = current_user.memories.build(name: params[:name], content: params[:content])
-      @memory.emotion_ids = params[:emotions]
-      if @memory.save
-        redirect to "/memories/#{@memory.slug}"
-      else
+    if logged_in?
+      if params[:name] == "" || params[:content] == ""
         redirect to "/memories/new"
+      else
+        @memory = current_user.memories.build(name: params[:name], content: params[:content])
+        @memory.emotion_ids = params[:emotions]
+        if @memory.save
+          redirect to "/memories/#{@memory.slug}"
+        else
+          redirect to "/memories/new"
+        end
       end
+    else
+      redirect to '/login'
     end
   end
   
   get '/memories/:slug' do
-    @memory = Memory.find_by_slug(params[:slug])
-    erb :'memories/show'
+    if logged_in?
+      @memory = Memory.find_by_slug(params[:slug])
+      erb :'memories/show'
+    else
+      redirect to '/login'
+    end
   end
 
   get '/memories/:slug/edit' do
-    @memory = Memory.find_by_slug(params[:slug])
-    if @memory && @memory.user == current_user
-      @emotions = Emotion.all
-      erb :'memories/edit'
+    if logged_in?
+      @memory = Memory.find_by_slug(params[:slug])
+      if @memory && @memory.user == current_user
+        @emotions = Emotion.all
+        erb :'memories/edit'
+      else
+        redirect to '/memories'
+      end
     else
-      redirect to '/memories'
+      redirect to '/login'
     end
   end
 
   patch '/memories/:slug' do
-    @memory = Memory.find_by_slug(params[:slug])
-    if @memory && @memory.user == current_user
-    end
-    if @memory.save
-      redirect to "memories/#{@memory.slug}"
+    if logged_in?
+      @memory = Memory.find_by_slug(params[:slug])
+      if @memory && @memory.user == current_user
+      end
+      if @memory.save
+        redirect to "memories/#{@memory.slug}"
+      else
+        redirect to "/memories/#{@memory.slug}/edit"
+      end
     else
-      redirect to "/memories/#{@memory.slug}/edit"
+      redirect to '/login'
     end
   end
   
   delete '/memories/:slug/delete' do
-    @memory = Memory.find_by_slug(params[:slug])
-    if @memory && @memory.user == current_user
-      @memory.delete
+    if logged_in?
+      @memory = Memory.find_by_slug(params[:slug])
+      if @memory && @memory.user == current_user
+        @memory.delete
+      end
+      redirect to '/memories'
+    else
+      redirect to '/login'
     end
-    redirect to '/memories'
   end
 end
