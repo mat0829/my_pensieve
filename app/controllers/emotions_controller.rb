7eada90cf62760eb90dada6a8d3c4b7c2f8c4c80
Mat Anderson <matandersonlv@hotmail.com>
2019-08-14 17:12:15 +0000

    added logged_in? to emotions_controller.rb

--- app/controllers/emotions_controller.rb
+++ app/controllers/emotions_controller.rb
@@ -1,15 +1,21 @@
 class EmotionsController < ApplicationController
   
-  get '/emotions' do 
-   @emotions = Emotion.all
-   
-   erb :'/emotions/index' 
+  get '/emotions' do
+   if logged_in?
+    @emotions = Emotion.all
+    erb :'/emotions/index'
+    else
+      redirect_to '/login'
+    end
   end
   
-  get '/emotions/:slug' do 
-    @emotion = Emotion.find_by_slug(params[:slug])
-
-    erb :'emotions/show'
+  get '/emotions/:slug' do
+    if logged_in?
+      @emotion = Emotion.find_by_slug(params[:slug])
+      erb :'emotions/show'
+    else
+      redirect_to '/login'
+    end
   end
   
 end
