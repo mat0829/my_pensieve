316cd87f6e1ca2e82ece5a1a3be12223de9d7fb3
Mat Anderson <matandersonlv@hotmail.com>
2019-08-14 23:51:46 +0000

    reverted back to previous emotions_controller code

--- app/controllers/emotions_controller.rb
+++ app/controllers/emotions_controller.rb
@@ -12,7 +12,7 @@ class EmotionsController < ApplicationController
   get '/emotions/:slug' do
     if logged_in?
       @emotion = Emotion.find_by_slug(params[:slug])
-      @emotion.memories = @emotion.memories.where(user_id: current_user.id)
+      @emotion = @emotion.memories.where(user_id: current_user.id)
       erb :'emotions/show'
     else
       redirect_to '/login'
