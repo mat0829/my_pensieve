c3130a1f84bb04eccd661ab6795f0781b6f61a2e
Mat Anderson <matandersonlv@hotmail.com>
2019-08-26 21:45:23 +0000

    added edit and patch to emotions_controller and added emotions/edit.erb

--- app/controllers/emotions_controller.rb
+++ app/controllers/emotions_controller.rb
@@ -19,6 +19,33 @@ class EmotionsController < ApplicationController
     end
   end
   
+  get '/emotions/:slug/edit' do
+    if logged_in?
+      if params[:name] == ""
+        redirect to "/emotions/#{params[:slug]}/edit"
+      else
+      @emotion = Emotion.find_by_slug(params[:slug])
+      erb :'emotions/edit'
+      end
+    else
+      redirect to '/login'
+    end
+  end
+  
+  patch '/emotions/:slug' do
+    if logged_in?
+      if params[:name] == ""
+        redirect to "/emotions/#{params[:slug]}/edit"
+      else
+        @emotion = Emotion.find_by_slug(params[:slug])
+        @emotion.update(name: params[:name])
+        redirect to "/emotions/#{@emotion.slug}"
+      end
+    else
+      redirect to '/login'
+    end
+  end
+  
   delete '/emotions/:slug/delete' do
     if logged_in?
       @emotion = Emotion.find_by_slug(params[:slug])
